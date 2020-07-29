b1ccfbff795b57d21fcb45cd7058245b7192ddf2
Voldemort79 <60815928+Voldemort79@users.noreply.github.com>
2020-07-29 16:03:03 +0430

    Created By Alireza

---
+++ app/src/main/java/com/example/bugatti/Activity/EmptyActivity.java
@@ -0,0 +1,22 @@
+package com.example.bugatti.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bugatti.R;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+
+    }
+
+    private void mta(){
+
+    }
+
+}
